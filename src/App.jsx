42f72c23e0a26850f63305e716676b8913e08156
itsechi <wardulinskao@gmail.com>
2023-03-17 15:03:17 +0100

    Check coords in the firestore

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import "./App.css";
 import { Header } from "./components/Header";
 import { Gameboard } from "./components/Gameboard";
 import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  doc,
+  setDoc,
+  getDoc,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAOkXWe2X7wgX8K3D5a44GLedUFRkP4UKY",
@@ -13,12 +23,24 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const checkCoords = async (x, y) => {
+  const coordsQuery = query(collection(db, "characters"), where("x", "==", x));
+  const querySnapshot = await getDocs(coordsQuery);
+  const docs = querySnapshot.docs[0];
+  if (docs) {
+    const character = docs.id;
+    const coords = docs.data();
+    console.log(character, coords);
+  }
+};
 
 export default function App() {
   return (
     <>
       <Header />
-      <Gameboard />
+      <Gameboard checkCoords={checkCoords} />
     </>
   );
 }
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,23 +1,24 @@
 import bg from "../assets/game.png";
-import char from "../assets/char.png";
+import emily from "../assets/emily.png";
+import elliott from "../assets/elliott.png";
+import haley from "../assets/haley.png";
 
-export const Gameboard = () => {
+export const Gameboard = (props) => {
   const relativeCoords = (e) => {
     const bounds = e.target.getBoundingClientRect();
     const x = e.clientX - bounds.left;
     const y = e.clientY - bounds.top;
-    console.log("x: " + (x / e.target.offsetWidth).toFixed(2));
-    console.log("y: " + (y / e.target.offsetHeight).toFixed(2));
-    console.log("x:" + x, "y:" + y);
-    // return {x: x, y: y};
+    const relX = (x / e.target.offsetWidth).toFixed(2);
+    const relY = (y / e.target.offsetHeight).toFixed(2);
+    props.checkCoords(relX, relY);
   };
 
   return (
     <main className="game">
       <div className="characters">
-        <img src={char}></img>
-        <img src={char}></img>
-        <img src={char}></img>
+        <img src={emily}></img>
+        <img src={elliott}></img>
+        <img src={haley}></img>
       </div>
       <div className="imgContainer" onClick={relativeCoords}>
         <img className="img" src={bg}></img>
