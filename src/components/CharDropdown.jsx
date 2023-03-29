fec2d8852f3d74e8fdd09e87f0910f37b9cb6922
itsechi <wardulinskao@gmail.com>
2023-03-29 13:07:25 +0200

    Remove characters from dropdown list after finding them

--- src/components/CharDropdown.jsx
+++ src/components/CharDropdown.jsx
@@ -7,21 +7,27 @@ export const CharDropdown = (props) => {
       style={{ top: props.coords.y, left: props.coords.x }}
       onClick={props.chooseCharacter}
     >
-      <img
-        className="character"
-        src={characters[0].img}
-        data-name={characters[0].name}
-      ></img>
-      <img
-        className="character"
-        src={characters[1].img}
-        data-name={characters[1].name}
-      ></img>
-      <img
-        className="character"
-        src={characters[2].img}
-        data-name={characters[2].name}
-      ></img>
+      {!characters[0].found && (
+        <img
+          className="character"
+          src={characters[0].img}
+          data-name={characters[0].name}
+        ></img>
+      )}
+      {!characters[1].found && (
+        <img
+          className="character"
+          src={characters[1].img}
+          data-name={characters[1].name}
+        ></img>
+      )}
+      {!characters[2].found && (
+        <img
+          className="character"
+          src={characters[2].img}
+          data-name={characters[2].name}
+        ></img>
+      )}
     </div>
   );
 };
